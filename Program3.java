4aa425e133b740f1e68e789c38c47849c11af94e
rajan2321x-55 <rajan2321x@gmail.com>
2026-02-07 09:59:26 +0530

    Add Program3.java to sort and print an array

---
+++ Program3.java
@@ -0,0 +1,13 @@
+package Array;
+import java.util.Arrays;
+public class Program3 {
+
+	public static void main(String[] args) {
+       int[] arr = {5, 3, 2, 4};
+        
+        Arrays.sort(arr);
+        
+        System.out.println(Arrays.toString(arr));
+	}
+
+}
