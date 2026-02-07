e80dec7f8e95152ced2c5038de4df31aca84ca44
rajan2321x-55 <rajan2321x@gmail.com>
2026-02-07 09:57:08 +0530

    Refactor main method in Program1.java

--- Program1.java
+++ Program1.java
@@ -3,10 +3,10 @@ package Array;
 public class Program1 {
 
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 3, 4}; // Sample Input
+		int[] arr = {1, 2, 3, 4}; 
         int sum = 0;
 
-        // The "for-each" loop: "For every 'num' in 'arr'..."
+       
         for (int num : arr) {
             sum += num;
         }
